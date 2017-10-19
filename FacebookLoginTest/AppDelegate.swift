edac83cc4658726d6a164812617fdd8e84d05942
h8254201 <h82542011@gmail.com>
2017-10-19 15:14:28 +0800

    init commit

--- FacebookLoginTest/AppDelegate.swift
+++ FacebookLoginTest/AppDelegate.swift
@@ -7,16 +7,22 @@
 //
 
 import UIKit
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
         return true
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -35,6 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBSDKAppEvents.activateApp()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
--- FacebookLoginTest/ViewController.swift
+++ FacebookLoginTest/ViewController.swift
@@ -7,11 +7,59 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,FBSDKLoginButtonDelegate {
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print("attempt to fetch profile......")
+        
+        let parameters = ["fields": "email, first_name, last_name, picture.type(large)"]
+        
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).start(completionHandler: {
+            connection, result, error -> Void in
+            
+            if error != nil {
+                print("登入失敗")
+                print("longinerror =\(error)")
+            } else {
+                
+                if let resultNew = result as? [String:Any]{
+                    
+                    print("成功登入")
+                    
+                    let email = resultNew["email"]  as! String
+                    print(email)
+                    
+                    let firstName = resultNew["first_name"] as! String
+                    print(firstName)
+                    
+                    let lastName = resultNew["last_name"] as! String
+                    print(lastName)
+                    
+                    if let picture = resultNew["picture"] as? NSDictionary,
+                        let data = picture["data"] as? NSDictionary,
+                        let url = data["url"] as? String {
+                        print(url) //臉書大頭貼的url, 再放入imageView內秀出來
+                    }
+                }
+            }
+        })
+        print("Y")
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("N")
+    }
+    func loginButtonWillLogin(_ loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        let bt = FBSDKLoginButton()
+        bt.center = self.view.center
+        bt.readPermissions = ["public_profile", "email", "user_friends"]
+        bt.delegate = self
+        self.view.addSubview(bt)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +68,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
